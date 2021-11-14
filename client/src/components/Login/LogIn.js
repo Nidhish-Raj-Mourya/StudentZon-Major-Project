2ccbc0808691be47c8d242bedcc29df9942e1fd3
Nidhish Raj Mourya <nidhishindoremp@gmail.com>
2021-11-14 15:34:56 +0530

    minor fixes

--- client/src/components/Login/LogIn.js
+++ client/src/components/Login/LogIn.js
@@ -5,6 +5,7 @@ import { authorize } from '../../utils/authorize'
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 import { logInUser } from './../../actions/userActions';
+import bg1 from './img/bg1.png';
 
 
 class LogIn extends Component {
@@ -68,35 +69,93 @@ class LogIn extends Component {
         <strong>Error: </strong> {this.state.errormsg}
       </div>
     ) : ('');
-    return (
-      <div className="container-login100">
+    
+      return (
+
         
-        {msgBlock}
-        {errBlock}
-        
-        {/* <span><h1 id="title">Shri Vaishnav Vidyapeeth Vishwavidyalaya </h1> </span> <br></br>
-        <span><h2 id="title2">Welcomes you to StudentZ<span role="img" aria-label="Earth">&#127759;</span>n </h2></span><br></br> */}
-        <form className="loginform popup"  onSubmit={this.handleSubmit}>
-          { <></>}
-          <div className="form-group">
-            <div><span><h1 id="title">Shri Vaishnav Vidyapeeth Vishwavidyalaya </h1> </span>
-        <span><h2 id="title2">Welcomes you to StudentZ<span role="img" aria-label="Earth">&#127759;</span>n </h2></span><br></br></div>
-            <div className="wrap-input100 validate-input" data-validate="Enter username">
+
+        <div className="container-register">
+
+<span><h3 id="title">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Shri Vaishnav Vidyapeeth Vishwavidyalaya&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</h3> </span> <br></br>
+
+
+          <div className="error">
+            {errBlock}
+          </div>
+          <div className="card mb-3 w-95 " id="card">
+            <div className="row no-gutters">
+              <div className="col-xs-12 col-sm-12 col-md-12 col-lg-8" >
+                <div className="bd-example">
+                  <div className="flex-centered">
+
+
+        {/* <span><h2 id="title2">Welcomes you to StudentZ<span role="img" aria-label="Earth">&#127759;</span>n </h2></span><br></br>  */}
+
+                    <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
+  
+                      <ol className="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                      </ol>
+  
+                      <div className="carousel-inner">
+  
+                        <div className="carousel-item active">
+                          <img src={bg1} className="d-block w-100" alt="Responsive" />
+                          <div className="carousel-caption d-none d-md-block">
+                            {/* <h2>Your one-stop destination for books</h2> */}
+                          </div>
+                        </div>
+  
+                        
+  
+                        
+                      </div>
+  
+                      <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                      </a>
+  
+                      <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+              <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+                <div className="card-body">
+                  <form className="registerform" onSubmit={this.handleSubmit}>
+                   
+
+<div className="form-group" data-validate="Enter username">
               <label htmlFor="Username">&emsp; Username</label>
               <input type="text" className="form-control" name="username" aria-describedby="emailHelp" placeholder="Username" />
               <span className="focus-input100" data-placeholder="&#xf207;"></span>
             </div>
+
+
             <div className="form-group">
               <br></br>
               <label htmlFor="Password">&emsp;Password</label>
               <input type="password" className="form-control" name="password" placeholder="Password" />
             </div>
+
             <button type="submit" className="btn btn-primary" id="index-submit">Submit</button>
             <span className="psw">Don't have an account? <Link to="/signup" id="forgot"> Register now.</Link></span>
+
+
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
-        </form>
-      </div>
-    )
+        </div>
+      )
   }
 }
 
