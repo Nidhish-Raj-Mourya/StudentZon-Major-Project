9ea7db1bf9a8c4d37074b556a27600fd4b856df2
Mahak-Bhansali <mahakbhansali32@gmail.com>
2021-11-17 22:21:12 +0530

    deleted default server

--- app.js
+++ app.js
@@ -140,6 +140,12 @@ app.post('/removeitem', (req, res) => {
 if(process.env.NODE_ENV =='production')
 {
   app.use(express.static('client/build'));
-}
+  const path=require("path");
+  app.get("*",(req,res)=>
+  {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+  }
+
 app.listen(process.env.PORT || 8000);
 console.log(`Listening to port ${port}`);
